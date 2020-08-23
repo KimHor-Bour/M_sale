675092bbee9f590fdbd9d1b32f2bb7aa069c3654
KimHor_Bour <bourkimhor@gmail.com>
2020-08-24 05:04:48 +0700

    KessSeller

---
+++ KessSeller/app/src/main/java/com/example/kessseller/Java/Recycler1.java
@@ -0,0 +1,13 @@
+package com.example.kessseller.Java;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Recycler1 extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
